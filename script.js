708dd971156311526fdac9ba69f3a289f6441112
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-11-15 10:29:53 -0600

    Added logic to check for wins

--- script.js
+++ script.js
@@ -20,16 +20,21 @@
  * 
  */
 
-const currentPlayer = document.querySelector('h3'),
-      btnReset = document.querySelector('button'),
-      boardContainer = document.getElementById('board-container'),
-      cells = document.querySelectorAll('.cell');
 
 const gameController = (function() {
+   const currentPlayer = document.querySelector('h3'),
+         btnReset = document.querySelector('button'),
+         cells = document.querySelectorAll('.cell');
    const playerOne = createPlayer('X'),
          playerTwo = createPlayer('O');
 
    playerOne.turn = true;
+
+   cells.forEach(cell => {
+      cell.addEventListener('click', makePlay);
+   });
+
+   btnReset.addEventListener('click', reset());
 
    function createPlayer(sign) {
       return {
@@ -56,30 +61,50 @@ const gameController = (function() {
       return playerOne.turn === true ? playerOne.sign : playerTwo.sign;
    }
 
-   cells.forEach(cell => {
-      cell.addEventListener('click', makePlay);
-   });
-      
-   function makePlay(e) {
-      const cell = e.target;
-      if(getCurrentPlayer() === 'X') {
-         cell.classList.add('letter');
+   function updateScreen(cell, player) {
+      if(player === 'X') {
+         cell.classList.add('letter', 'x');
          const X = document.createElement('h1');
          X.textContent = 'X';
          cell.appendChild(X);
-         alternateTurns();
-         updateTurnDiv();
-         cell.removeEventListener('click', makePlay);
-      } else if (getCurrentPlayer() === 'O') {
-         cell.classList.add('letter');
+      } else if (player === 'O') {
+         cell.classList.add('letter', 'o');
          const O = document.createElement('h1');
          O.textContent = 'O';
          cell.appendChild(O);
-         alternateTurns();
-         updateTurnDiv();
-         cell.removeEventListener('click', makePlay);
+      }
+   }
+
+   function makePlay(e) {
+      const cell = e.target;
+      let currentPlayer = getCurrentPlayer();
+
+      updateScreen(cell, currentPlayer);
+      gameboard.addPlayer(getCellIndex(cell), currentPlayer);
+      
+      alternateTurns();
+      updateTurnDiv();
+      cell.removeEventListener('click', makePlay);
+
+      if(gameboard.checkWin(currentPlayer)) {
+         setTimeout(() => {
+            alertWinner(currentPlayer);
+         }, 1)
+         reset();
       }
    };
+
+   function getCellIndex(currentCell) {
+      return currentCell.getAttribute('index');
+   }
+
+   function reset() {
+      gameboard.resetBoard();
+   }
+
+   function alertWinner(currentPlayer) {
+      alert(`${currentPlayer} won!`);
+   }
 
    return;
 })();
@@ -90,8 +115,45 @@ const gameboard = (function() {
       '','','',
       '','',''
    ]
-   return { board };
-})
+
+   const winningPatterns = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+   ]
+
+   function resetBoard() {
+      for(let i = 0; i < 9; i++)
+         board[i] = '';
+   }
+
+   function addPlayer(index, currentPlayer) {
+      board[index] = currentPlayer;
+   }
+
+   function checkWin(currentPlayer) {
+      let indexOne = 0,
+          indexTwo = 0,
+          indexThree = 0;
+
+      for(let i = 0; i < 9; i++) {
+         indexOne = winningPatterns[i][0];
+         indexTwo = winningPatterns[i][1];
+         indexThree = winningPatterns[i][2];
+
+         if(board[indexOne] === currentPlayer &&
+            board[indexTwo] === currentPlayer &&
+            board[indexThree] === currentPlayer)
+               return true;
+      }
+   }
+
+   return { addPlayer, checkWin, resetBoard };
+})();
 
 gameController();
-
